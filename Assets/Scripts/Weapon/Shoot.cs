70cefe4220eeb3435bbba90671bad0d599955a34
AldairNM <navarromachado76@gmail.com>
2022-04-27 14:03:40 -0500

    Animaciones de Disparo y Apuntado

--- Assets/Scripts/Weapon/Shoot.cs
+++ Assets/Scripts/Weapon/Shoot.cs
@@ -9,6 +9,9 @@ public class Shoot : MonoBehaviour
     public float shootForce = 1500f;
     public float shootRate = 0.3f;
     private float shootRateTime = 0;
+    public Animator Animacion;
+    public GameObject AnimacionCamara;
+    int x=1;
     void Start()
     {
         
@@ -17,17 +20,64 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            if (Time.time > shootRateTime && GameManager.Instance.ammo > 0)
+            x = 0;
+            Animacion.SetBool("apuntar", true);
+            AnimacionCamara.GetComponent<Animator>().SetBool("apuntadoCam", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            x = 1;
+            Animacion.SetBool("apuntar", false);
+            AnimacionCamara.GetComponent<Animator>().SetBool("apuntadoCam", false);
+        }
+        disparoClickIzquierdo(x);
+    }
+    public void disparo()
+    {
+        GameManager.Instance.ammo--;
+        GameObject newBullet;
+        newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+        newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shootForce);
+        shootRateTime = Time.time + shootRate;
+        Destroy(newBullet, 5);
+    }
+    public int disparoClickIzquierdo(int y)
+    {
+        if (y == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                GameManager.Instance.ammo--;
-                GameObject newBullet;
-                newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
-                newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shootForce);
-                shootRateTime = Time.time + shootRate;
-                Destroy(newBullet, 5);
+                if (Time.time > shootRateTime && GameManager.Instance.ammo > 0)
+                {
+                    Animacion.SetBool("disparo", true);
+                    Animacion.SetBool("correr", false);
+                    disparo();
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                Animacion.SetBool("disparo", false);
             }
         }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                if (Time.time > shootRateTime && GameManager.Instance.ammo > 0)
+                {
+                    Animacion.SetBool("dispararapuntar", true);
+                    Animacion.SetBool("correr", false);
+                    disparo();
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                Animacion.SetBool("dispararapuntar", false);
+            }
+        }
+        return 0;
     }
+
 }
